9c5cea40da91e75ae9da993da589f07149d23ef5
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-11-23 20:23:23 -0300

    tirinhas

---
+++ baixar_tirinhas.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+baixa = requests.get("https://c.xkcd.com/random/comic/")
+
+soup = BeautifulSoup(baixa.text,'html.perser')
+url_img = soup.select('div#comic img')[0]('src')
+print(url_img)
+dowload_url = "https:{}".format(url_img)
+
+print("baixando: {}...".format(dowload_url))
+
+with open('xlcd-scrapped.png','wb') as image:
+  image.write(requests.get(dowload_url).content)
